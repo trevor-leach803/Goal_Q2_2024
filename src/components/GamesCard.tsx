73888b8f94b03810c524cb741d7cc7eefd88e470
Trevor Leach <taleach2016@gmail.com>
2024-06-23 14:17:32 -0400

    Remove duplicate style

--- src/components/GamesCard.tsx
+++ src/components/GamesCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GamesCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"} width="300px">
+    <Card minHeight={"350px"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
